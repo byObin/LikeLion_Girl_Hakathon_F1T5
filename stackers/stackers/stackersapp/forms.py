0b79b37f9c41091a266e201c2701e681b730deee
elive7 <parksub020704@gmail.com>
2022-07-30 01:03:49 +0900

    Revert "party 페이지 다시"
    This reverts commit 499d240b39c5b387f1570f7b2a12252a45d0ba33, reversing
    changes made to 67ee6e90dafc8e8f5a8710b9fcaf7b0f415039d3.

--- stackers/stackers/stackersapp/forms.py
+++ stackers/stackers/stackersapp/forms.py
@@ -1,10 +1,3 @@
 from django import forms
 from .models import Develop
 from .models import Party
-
-# Develop 모델을 기반으로 form 생성
-class DevelopModelForm(forms.ModelForm):
-    class Meta:
-        model = Develop
-        # fields = '__all__'
-        fields = ['title', 'body']
