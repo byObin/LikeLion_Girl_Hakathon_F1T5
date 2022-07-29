166526eb28bd9b9aef335f0690847e0fa777636f
jiinkyung <jinkyung010310@gmail.com>
2022-07-30 01:03:55 +0900

    create qna

--- stackers/stackers/stackers/urls.py
+++ stackers/stackers/stackers/urls.py
@@ -1,21 +1,16 @@
-"""stackers URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from stackersapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('develop/', views.home, name='home'), 
+    path('detail/<int:develop_id>', views.detail, name='detail'),  # 아이디어 디벨롭/세부 페이지 
+    path('modelformcreate/', views.modelformcreate, name='modelformcreate'), # 새로운 아이디어 추가
+    path('qna/', views.qna, name='qna'),
+    path('qna/<int:qna_id>', views.qna_detail, name='qna_detail'),
+] 
+# media 파일에 접근할 수 있는 url도 추가해주어야 함.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
