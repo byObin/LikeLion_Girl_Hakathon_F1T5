e76d5a64eeb6bfdeaef21d7d276105b71e0dd23e
jiinkyung <jinkyung010310@gmail.com>
2022-07-29 22:16:02 +0900

    commit

--- stackers/stackers/stackers/urls.py
+++ stackers/stackers/stackers/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from stackersapp import Develop
+from stackersapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('detail/<int:develop_id>', views.detail, name='detail'),   
 ]
+# media 파일에 접근할 수 있는 url도 추가해주어야 함.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stackers/stackers/stackersapp/views.py
+++ stackers/stackers/stackersapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-
+from .models import Develop
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 def home(request):
     # 블로그 글을 모두 띄우는 코드 (DB의 블로그 객체들을 모두 가져옴)
     # posts = Blog.objects.all()
-    posts = Blog.objects.filter().order_by('-date') # 내가 가져오고자 하는 정보를 필터링 해서 가져옴 / order_by: 정렬
+    posts = Develop.objects.filter().order_by('-date') # 내가 가져오고자 하는 정보를 필터링 해서 가져옴 / order_by: 정렬
     return render(request, 'index.html', {'posts' : posts})
 
-def detail(request, blog_id):
+def detail(request, develop_id):
     # develop_id 번째 게시글을 DB로부터 가져와서
     develop_detail = get_object_or_404(Develop, pk=develop_id)
 
